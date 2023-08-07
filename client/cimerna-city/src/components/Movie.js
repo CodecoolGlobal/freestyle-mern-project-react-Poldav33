644ba38265a4ad6c2c5a4d8e867bfa2bd9b1225c
Poldav33 <poldav33@gmail.com>
2023-08-07 22:43:06 +0200

    repair cursor hovers

--- client/cimerna-city/src/components/Movie.js
+++ client/cimerna-city/src/components/Movie.js
@@ -1,17 +1,19 @@
 const Movie = ({ movie, onClick }) => {
-  return (<div key={movie["_id"]} className="movie-card">
+  return (
+  <div key={movie["_id"]} className="movie-card">
     <img src={movie.Poster} alt="Poster" className="movie-poster"/>
     <div className="movie-details-container">
       <div className="movie-title" onClick={() => onClick(movie)}> {movie.Title} </div>
       <div className="movie-minor-details">
-        <div>{movie.Rated}</div>
         <div>{movie.Genre}</div>
         <div>{movie.Runtime}</div>
+        {movie.Rated !== "Unrated" && <div>"{movie.Rated}" Rated</div>}
       </div>
-      <div>{movie.Language}</div>
-      <div>{movie.Plot}</div>
+      <div className="movie-language">Language: {movie.Language}</div>
+      <div className="shortplot">{movie.Plot}</div>
     </div>
-  </div>);
+  </div>
+  );
 }
 
 export default Movie;
