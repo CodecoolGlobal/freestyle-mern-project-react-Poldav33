6924a25c00957b6b02ef8c797e388553c14884c8
Poldav33 <poldav33@gmail.com>
2023-08-08 11:31:02 +0200

    create basic structure of ScheduleMovie

--- client/cimerna-city/src/components/ScheduleMovie.js
+++ client/cimerna-city/src/components/ScheduleMovie.js
@@ -1,8 +1,16 @@
-function ScheduleMovie() {
+function ScheduleMovie({ handleSubmit }) {
     return (
-        <div>
-            
-        </div>
+        <form onSubmit={handleSubmit}>
+            <label>
+                Movie Title: <input type="text"/>
+            </label>
+            <label>
+                Date: <input type="date"/>
+            </label>
+            <label>
+                Seats: <input type="number"/>
+            </label>
+        </form>
     );
 }
 
