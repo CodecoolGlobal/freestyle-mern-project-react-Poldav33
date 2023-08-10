2bd1397244f1909951c409bb80e6a9a321f9a713
Janos Szabo <128319453+SilentJJ@users.noreply.github.com>
2023-08-10 17:43:46 +0200

    fix delete button bug

--- client/cimerna-city/src/components/EditMovie.js
+++ client/cimerna-city/src/components/EditMovie.js
@@ -1,23 +1,41 @@
 import MovieDetails from "./MovieDetails";
 import EditSchedule from "./EditSchedule";
+import { useState } from "react";
 
 function EditMovie ({ movie }) {
-  const movieCopy = Object.assign({}, movie)
-  movieCopy.Schedule.sort((a, b) => a.fullDate - b.fullDate)
-  const saveNewSchedule = (id) => {
-    movieCopy.Schedule.sort((a, b) => a.fullDate - b.fullDate)
+  
+  movie.Schedule.sort((a, b) => a.fullDate - b.fullDate)
+  const [newSchedule, setNewSchedule] = useState(movie.Schedule)
+
+  const updateDB = (id) => {
     fetch(`/api/movies/${id}`, {
       method: 'PUT',
       headers: { 'Content-type': 'application/json' },
-      body: JSON.stringify({ newData: [...movieCopy.Schedule] })
+      body: JSON.stringify({ newData: [...movie.Schedule] })
     })
+    setNewSchedule(movie.Schedule)
+    console.log(movie.Schedule)
   }
-  return  <>
+
+  const saveNewSchedule = (id) => {
+    movie.Schedule.sort((a, b) => a.fullDate - b.fullDate)
+    updateDB(id)
+  }
+
+  const deleteSchedule = (id) => {
+    movie.Schedule = movie.Schedule.filter(schedule => schedule['_id'] !== id)
+    updateDB(id)
+  }
+
+  return  (<>
       <MovieDetails movie={movie}/>
-    {movieCopy.Schedule.map((schedule, index) => 
-      <EditSchedule key={schedule.title} index={index} schedule={schedule} onSave={saveNewSchedule}/>
+      {newSchedule.map((schedule, index) => 
+        <>
+          <EditSchedule key={schedule['_id'] + index} index={index} schedule={schedule} onSave={saveNewSchedule} />
+          <button key={schedule['_id']} onClick={() => deleteSchedule(schedule['_id'])}>Delete</button>
+        </>
       )}
-      </>
+      </>)
 
 }
 
