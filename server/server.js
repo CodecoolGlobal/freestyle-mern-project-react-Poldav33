43651e3f26811e0584ab2478a2da05ff1f0f6db0
Poldav33 <poldav33@gmail.com>
2023-08-10 13:20:28 +0200

    create new endpoint to filter movies

--- server/server.js
+++ server/server.js
@@ -15,6 +15,15 @@ app.get('/api/movies', async (req ,res) => {
     }
 })
 
+app.get('/api/movies/filter/,', async (req,res) => {
+    try {
+        const filteredMovies = await Movie.find({ Schedule : { date : req.body.date} });
+        res.send(filteredMovies);
+    } catch (error) {
+        console.log(error);
+    }
+} );
+
 app.post('/api/movie/add', (req, res) => {
     
     const { 
