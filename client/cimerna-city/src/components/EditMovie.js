1b967d91e86e1af8971d28419b404f267021cddf
Janos Szabo <128319453+SilentJJ@users.noreply.github.com>
2023-08-10 13:25:57 +0200

    create save edited schedule server endpoint and create a request for it

--- client/cimerna-city/src/components/EditMovie.js
+++ client/cimerna-city/src/components/EditMovie.js
@@ -2,14 +2,20 @@ import MovieDetails from "./MovieDetails";
 import EditSchedule from "./EditSchedule";
 
 function EditMovie ({ movie }) {
-  console.log(movie)
   const movieCopy = Object.assign({}, movie)
   movieCopy.Schedule.sort((a, b) => a.fullDate - b.fullDate)
-  console.log(movieCopy)
+  const saveNewSchedule = (id) => {
+    movieCopy.Schedule.sort((a, b) => a.fullDate - b.fullDate)
+    fetch(`/api/movies/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify({ newData: [...movieCopy.Schedule] })
+    })
+  }
   return  <>
       <MovieDetails movie={movie}/>
-    {movieCopy.Schedule.map(schedule => 
-      <EditSchedule schedule={schedule}/>
+    {movieCopy.Schedule.map((schedule, index) => 
+      <EditSchedule key={schedule.title} index={index} schedule={schedule} onSave={saveNewSchedule}/>
       )}
       </>
 
